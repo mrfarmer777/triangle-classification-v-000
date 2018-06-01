a9234478cfdb0ece258abc0973b9af782e989f2c
Matt Farmer <mrfarmer777@gmail.com>
2018-06-01 04:52:43 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
 
   def kind
     if @sides.any? {|side| side<=0} || (@sone+@stwo)<@sthree
-      raise TriangleError      
+      raise TriangleError
     else
       if @sone==@stwo && @sone==@sthree
         :equilateral
