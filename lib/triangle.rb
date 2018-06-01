099056638ae8acde96b33940d44229959afa3395
Matt Farmer <mrfarmer777@gmail.com>
2018-06-01 04:36:21 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,10 +12,17 @@ class Triangle
     if @sone==@stwo && @sone==@sthree
       :equilateral
     elsif @sone==@stwo || @sone==@sthree || @stwo==@sthree
-      
+      :isosceles
+    elsif @sone!=@stwo && @stwo!=@sthree && @sone!=@sthree
+      :scalene
+    else
+      "Yo triangle fucked, brah."
+    end
+    
 
-      
-      
+
+
+
   end
 
   class TriangleError< StandardError
