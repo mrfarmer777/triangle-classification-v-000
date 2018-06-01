4f742370d0ba78cd76a87b896c788b7b453ec206
Matt Farmer <mrfarmer777@gmail.com>
2018-06-01 04:30:38 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
   # write code here
+  attr_accessor :sone, :stwo, :sthree
+
+  def initialize(sone,stwo,sthree)
+    @sone=sone
+    @stwo=stwo
+    @sthree=sthree
+  end
+
+  def kind
+
+  end
+
+  class TriangleError< StandardError
+    def message
+      "something"
+    end
+  end
+  
 end
